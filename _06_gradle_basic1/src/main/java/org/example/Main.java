3312004c20f416c182bed93f27c38e56afd62de5
electro9666 <electro9666@gmail.com>
2021-06-27 21:06:04 +0900

    add gradle basic

---
+++ _06_gradle_basic1/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.gradle.api.PersonList;
+
+public class Main {
+	public static void main(String[] args) {
+//		org.gradle.api.Project
+		PersonList personList = new org.gradle.api.PersonList();
+	}
+}
